a312c6f710253c11c9a7378c22b77ff0173ec3b7
Sigma <jskolska@edu.cdv.pl>
2024-06-26 01:20:19 +0200

    bomba

--- Assets/Scripts/Echo/EchoSphere.cs
+++ Assets/Scripts/Echo/EchoSphere.cs
@@ -8,29 +8,62 @@ public class EchoSphere : MonoBehaviour
     [SerializeField] private PlayerMovement playerMovement;
     [SerializeField] private float timeToWaitStepToEcho = 0.5f;
     [SerializeField] private float timeToWaitEchoToStep = 0.5f;
-    [SerializeField] private float spherecastRadius = 10f;
     [SerializeField] private LayerMask echoLayer;
+    public List<GameObject> echoObjects;
 
-    private void Update()
+    private void Start()
+    {
+       // StartCoroutine(Moving());
+    }
+    /*private void Update()
     {
         if (playerMovement.isMoving)
         {
+            Debug.Log("coroutine starting idk");
             StartCoroutine(Moving());
         }
         else
         {
+            Debug.Log("coroutine stopping idk");
             StopCoroutine(Moving());
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.layer);
+        if (other.gameObject.layer == echoLayer)
+        {
+            echoObjects.Add(other.gameObject);
+            Debug.Log("why");
         }
     }
 
-    IEnumerator Moving()
+    private void OnTriggerExit(Collider other)
     {
-        //play stepping sound
-        yield return new WaitForSeconds(timeToWaitStepToEcho);
-        //play echo sound
-        //spherecast layer/tag shaderobject
-        //turn on shaders
-        //turn on their scripts to turn off shaders
-        yield return new WaitForSeconds(timeToWaitEchoToStep);
+        if (other.gameObject.layer == echoLayer)
+        {
+            echoObjects.Remove(other.gameObject);
+        }
     }
+
+    /*IEnumerator Moving()
+    {
+        while(playerMovement.isMoving)
+        {
+            Debug.Log("schmoving");
+            //play stepping sound
+            yield return new WaitForSeconds(timeToWaitStepToEcho);
+            //play echo sound
+            foreach (GameObject echoObject in echoObjects)
+            {
+                if (echoObject == null) continue;
+                echoObject.GetComponent<Shutdown>().Fading();
+            }
+            yield return new WaitForSeconds(timeToWaitEchoToStep);
+        }
+
+        yield return new WaitWhile((() => !playerMovement.isMoving));
+        StartCoroutine(Moving());
+    }*/
 }
---
+++ Assets/Scripts/Echo/Shutdown.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Shutdown : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer.GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+    }
+
+    public void Fading()
+    {
+        StartCoroutine(Fadeout());
+    }
+
+    IEnumerator Fadeout()
+    {
+        meshRenderer.enabled = true;
+        //set color black
+        //dotween fade to white
+        yield return new WaitForSeconds(0.75f);
+        meshRenderer.enabled = false;
+    }
+
+    // private void OnDrawGizmos()
+    // {
+    //     Gizmos.color = Color.red;
+    //     Gizmos.DrawWireCube(GetComponent<BoxCollider>().center, GetComponent<BoxCollider>().size);
+    // }
+}
