70d325680c61921ab09a09338a0fc5c74c15ecc3
Sigma <jskolska@edu.cdv.pl>
2024-06-27 09:04:12 +0200

    .

--- Assets/Scripts/SequenceManager.cs
+++ Assets/Scripts/SequenceManager.cs
@@ -8,12 +8,20 @@ public class SequenceManager : MonoBehaviour
     [SerializeField] private DialogueTrigger firstDialogue;
     [SerializeField] private MinigameController minigame;
     [SerializeField] private DialogueTrigger secondDialogue;
+    [SerializeField] private bool isActuallySecond;
+
+    [SerializeField] private SequenceManager nextMemorySequence;
 
     private void Update()
     {
-        if (firstDialogue.isComplete() && !minigame.isComplete && !secondDialogue.isComplete())
+        if (firstDialogue.isComplete() && !minigame.isComplete && !secondDialogue.isComplete() && !isActuallySecond)
         {
             FirstDialogueToMinigame();
+        }
+        
+        else if (firstDialogue.isComplete() && isActuallySecond)
+        {
+            
         }
         // else if (firstDialogue.isComplete() && minigame.isComplete && !secondDialogue.isComplete())
         // {
